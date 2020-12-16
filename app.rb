9d89686b51b8a7ce9f47c9da23beb5d74ff371e3
Julian <julianrosenthal@yahoo.com>
2020-12-16 14:28:42 -0800

    commit tree 64b30958e990a2bc57ada540fda4adb22e209ed8

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is Julian"
+    end
+
+    get '/hometown' do 
+        "My hometown is New Haven"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Goodbye Blue Sky"
+    end
 end
